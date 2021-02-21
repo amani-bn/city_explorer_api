53540fb86e74cfe1dd7af5dea9f3fc9e4f5b7186
amani-bn <amanialabdallah@yahoo.com>
2021-02-21 17:24:01 +0200

    lab 6 done

---
+++ README.md
@@ -0,0 +1,26 @@
+# city_explorer_api
+
+## Author: Amani bani naser .
+## version: 1.0.0
+
+## Overview
+its backend lab to get data from existence files inside project
+
+## Getting Started
+1- create a new repo for this lab .
+2- satrt with get data for location from location.json.
+3- get data for weather from weather.json.
+4- handle with error .
+
+## Architecture
+javaScript (Node.js)
+
+## Change Log
+  for location it take 1 hour.
+  for weather it take 1 hour and 20 min.
+  for error 5 min
+  21-2-2021 4:30pm - Application now has a fully-functional express server, with a GET route for the all features .
+
+  ## Credits and Collaborations
+  TA. shihab eshtaiwi
+
---
+++ server.js
@@ -0,0 +1,66 @@
+"use strict";
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const server = express();
+server.use(cors());
+const PORT = process.env.PORT || 3000;
+// location route localhost:3000/location
+server.get("/location", (req, res) => {
+  const locData = require("./data/location.json");
+  // console.log(locData);
+  // console.log(locData[0]);
+  const locObj = new Location(locData);
+  // console.log(locData[0]);
+
+  res.send(locObj);
+});
+
+function Location(locJson) {
+  this.search_query = "Lynnwood";
+  this.formatted_query = locJson[0].display_name;
+  this.latitude = locJson[0].lat;
+  this.longitude = locJson[0].lon;
+}
+// another route for weather localhost:3000/weather
+const weathArr = [];
+
+server.get("/weather", (req, res) => {
+  const wethData = require("./data/weather.json");
+  // console.log (wethData);
+  wethData.data.forEach((element) => {
+    
+    const wethObj = new Weather(element);
+    console.log(wethObj);
+  });
+  res.send(weathArr);
+});
+
+function Weather(wethJson) {
+  // wether constructor
+
+  this.forecast = wethJson.weather.description;
+  this.time = wethJson.valid_date;
+
+  weathArr.push(this);
+
+  // [
+  //     {
+  //       "forecast": "Partly cloudy until afternoon.",
+  //       "time": "Mon Jan 01 2001"
+  //     },
+  //     {
+  //       "forecast": "Mostly cloudy in the morning.",
+  //       "time": "Tue Jan 02 2001"
+  //     },
+  //     ...
+  //   ]
+}
+// function to handle error
+server.use('*',(req,res)=>{
+    res.status(500).send('Sorry, something went wrong')
+})
+
+server.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
