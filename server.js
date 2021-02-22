812da174229b2c694a7c54f1cc11cc99c6635267
amani-bn <amanialabdallah@yahoo.com>
2021-02-23 00:58:40 +0200

    lab 7 done

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const express = require("express");
 const cors = require("cors");
 const superagent = require("superagent");
 
-// Application Setyp
+// Application Setup
 const PORT = process.env.PORT || 3000;
 const server = express();
 server.use(cors());
 
 // Route Definitions
 server.get("/location", locationHandler);
-// server.get("/weather",weatherHandler);
-// server.get("/park",parkHandler);
-// server.get("*",notFoundHandler);
-// server.use(errorHandler);
+server.get("/weather", weatherHandler);
+server.get("/parks", parkHandler);
+server.get("/*", errorHandler);
+// server.use(handleError);
 
 function locationHandler(req, res) {
   const cityName = req.query.city;
@@ -34,19 +34,48 @@ function locationHandler(req, res) {
       errorHandler(`Error`, req, res);
     });
 }
+function weatherHandler(req, res) {
+  // https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=API_KEY
+  const cityName = req.query.search_query;
 
-// location route localhost:3000/location
-// server.get("/location", (req, res) => {
-// const locData = require("./data/location.json");
-// console.log(locData);
-//   // console.log(locData[0]);
-//   const cityName=
-//   const locObj = new Location(locData);
-//   // console.log(locData[0]);
+  let key = process.env.WEATHER_KEY;
+  let url = `http://api.weatherbit.io/v2.0/forecast/daily?city=${cityName},NC&key=${key}`;
+  superagent
+    .get(url)
+    .then((weatData) => {
+      let weathArr = weatData.body.data.map((value) => {
+        // const wethObj = new Weather(value);
+        // console.log(weathArr);
+        return new Weather(value);
+        // const wethObj = new Weather(weatData);
+      });
+      res.send(weathArr);
 
-//   res.send(locObj);
-// });
+      console.log(weathArr);
+    })
+    .catch(() => {
+      errorHandler(`Error`, req, res);
+    });
+}
 
+function parkHandler(req, res) {
+  // https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=wNBehwNfBlbKWvhCgiCjwV5ZFDSTLmCwwbxfVgqd
+
+  let key = process.env.PARKS_API_KEY;
+  let url = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${key}`;
+  superagent
+    .get(url)
+    .then((parkData) => {
+      const parkArr = parkData.body.data.map((value) => {
+        const parkObj = new Park(value);
+        return parkObj;
+      });
+      res.send(parkArr);
+    })
+    .catch(() => {
+      errorHandler(`Error`, req, res);
+    });
+}
 // constructors
 function Location(city, locJson) {
   this.search_query = city;
@@ -54,38 +83,27 @@ function Location(city, locJson) {
   this.latitude = locJson.lat;
   this.longitude = locJson.lon;
 }
-// another route for weather localhost:3000/weather
-// const weathArr = [];
 
-server.get("/weather", (req, res) => {
-  const wethData = require("./data/weather.json");
-  // console.log (wethData);
-
-  let weathArr = wethData.data.map((value) => {
-    const wethObj = new Weather(value);
-    // console.log(weathArr);
-    return wethObj;
-  });
-  // wethData.data.forEach((element) => {
-  // console.log(wethObj);
-  res.send(weathArr);
-});
 
 function Weather(wethJson) {
-  // wether constructor
-
   this.forecast = wethJson.weather.description;
   this.time = new Date(wethJson.valid_date).toDateString();
-  // weathArr.push(this);
 }
-// function to handle error
-server.use("*", (req, res) => {
-  let errorObj = {
-    status: 500,
-    responsetext: "Sorry, something went wrong",
-  };
-  res.send(errorObj);
-});
+
+
+function Park(geoData) {
+  this.name = geoData.fullName;
+  this.address = `${geoData.addresses[0].line1}${geoData.addresses[0].city}${geoData.addresses[0].stateCode}${geoData.addresses[0].postalCode}`;
+  this.fee = geoData.entranceFees[0].cost;
+  this.description = geoData.description;
+  this.url = geoData.url;
+}
+
+function errorHandler(errors) {
+  server.use("*", (req, res) => {
+    res.status(500).send(errors);
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
